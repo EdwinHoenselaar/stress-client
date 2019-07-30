0a5943b842f93d344b554bb4ad3461c407125089
EdwinHoenselaar <EdwinHoenselaar@gmail.com>
2019-07-30 17:23:05 +0200

    basic setup

---
+++ src/components/QMSMain.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-8">
+        <h1>Stress</h1>
+      </div>
+      <div class="col-4">
+        <button type="button" class="btn btn-info new">
+          Add new question
+        </button>
+      </div>
+    </div>
+    <div class="row">
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QMSMain',
+}
+</script>
+
+<style scoped>
+  .new {
+    margin-top: 2rem;
+  }
+</style>
